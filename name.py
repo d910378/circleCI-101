da87e1de380f95af2e2e3d616515d76895fa042f
d910378 <60721068+d910378@users.noreply.github.com>
2020-02-17 16:16:44 +1100

    Create name.py

---
+++ name.py
@@ -0,0 +1,10 @@
+name = 'Joe'
+
+if name == 'Fred':
+    print('Hello Fred')
+elif name == 'Xander':
+    print('Hello Xander')
+elif name == 'Joe':
+    print('Hello Joe')
+else:
+    print("I don't know who you are!")
